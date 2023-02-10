226f12d48532c7e3b39c829efd62e9a3c89af182
SapoPepe <116886407+SapoPepe@users.noreply.github.com>
2023-02-10 22:37:24 +0100

    Update deslizante.js

--- js/deslizante.js
+++ js/deslizante.js
@@ -1,11 +1,29 @@
 const slider = document.querySelector('.deslizante');
-let contador = 0;
+const contenedor_firefox = document.querySelector('.deslizante-firefox');
+const navegador = navigator.userAgent;
+let counter = 0;
 
-setInterval(() => {
-    contador++;
-    slider.style.transform = `translateX(-${contador * 100}%)`;
-    if (contador === slider.children.length - 1) {
-        contador = -1;
-    }
-}, 3000);
+
+
+if (navegador.indexOf("Firefox") !== -1) {
+    slider.style.display = "none";
+    contenedor_firefox.style.display = "flex";
+    setInterval(() => {
+        counter++;
+        contenedor_firefox.style.transform = `translateX(-${counter * 100}%)`;
+        if (counter === contenedor_firefox.children.length - 1) {
+            counter = -1;
+        }
+    }, 3000);
+}
+
+else {
+    setInterval(() => {
+        counter++;
+        slider.style.transform = `translateX(-${counter * 100}%)`;
+        if (counter === slider.children.length - 1) {
+            counter = -1;
+        }
+    }, 2000);
+}
 
