00dfd274ed02938d20c20e7eabc2d8ff730299e7
SapoPepe <116886407+SapoPepe@users.noreply.github.com>
2023-08-01 17:48:09 +0200

    Delete js directory

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var btnDesplegable = document.querySelector('.btn-desplegable');
-    var contenidoDesplegable = document.querySelector('.contenido-desplegable');
-
-    btnDesplegable.addEventListener('click', function() {
-        contenidoDesplegable.classList.toggle('mostrar');
-    });
-});
